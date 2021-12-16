220928352316a5774c628f292f125c77702295ec
Pedro Igor <pedroicandido1@gmail.com>
2021-12-16 16:47:21 -0300

    feat: ensure PointReducer return default state if no case type match

---
+++ src/store/reducers/Point.js
@@ -0,0 +1,13 @@
+const initialState = {
+  currentPoint: {},
+  historyPoints: [],
+};
+
+const pointReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default pointReducer;
